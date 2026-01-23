725c6c4373aa39605121602237fc00ee98cdea26
kkh <kkhyungyung0@naver.com>
2026-01-23 13:17:28 +0900

    [Chore] test 관련 convention 수정

--- build-logic/convention/src/main/java/com/kkh/convention/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/java/com/kkh/convention/AndroidLibraryConventionPlugin.kt
@@ -1,33 +1,32 @@
-package com.kkh.convention/*
- * Copyright 2022 The Android Open Source Project
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *       https://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
+package com.kkh.convention
 
 import com.android.build.gradle.LibraryExtension
 import com.kkh.convention.extensions.configureKotlinAndroid
 import com.kkh.convention.extensions.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.testing.Test
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.withType
 
 class AndroidLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
         apply(plugin = "com.android.library")
         apply(plugin = "org.jetbrains.kotlin.android")
         apply(plugin = "de.mannodermaus.android-junit5")
+
+        // module에 test 관련 파일 없으면 테스트 진행하지 않는다.
+        tasks.withType<Test>().configureEach {
+            failOnNoDiscoveredTests.set(false)
+
+            // test file에 없어도, 폴더 생성. test 통과용.
+            doFirst {
+                reports.html.outputLocation.get().asFile.mkdirs()
+                reports.junitXml.outputLocation.get().asFile.mkdirs()
+            }
+        }
 
         extensions.configure<LibraryExtension> {
             configureKotlinAndroid(this)
